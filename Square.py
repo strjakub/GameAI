89ba3786340dc9dd10d5cb74e39e572b9922526e
DESKTOP-I7NIUE3\stroj <jakubstr@student.agh.edu.pl>
2022-04-04 23:25:31 +0200

    pre-engine

--- Square.py
+++ Square.py
@@ -1,18 +1,34 @@
 from Vector import Vector
+from Map import Map
 
 
 class Square:
-    jumpPower = Vector(0, 5)
+    jumpPower = Vector(0, -5)
+    gravity = Vector(0, 1)
 
-    def __init__(self, vector: Vector):
+    def __init__(self, vector: Vector, board: Map):
         self.position = vector
         self.velocity = Vector(1, 0)
+        self.map = board
 
     def __str__(self):
-        return f"position: {self.position}\nvelocity: {self.velocity}"
+        return f"position: {self.position}\nvelocity: {self.velocity}\n{self.isStable()}"
 
     def jump(self):
-        self.velocity.add(Square.jumpPower)
+        if self.isStable():
+            self.velocity.add(Square.jumpPower)
 
-    
+    def move(self):
+        self.position.add(self.velocity)
+        if not self.isStable():
+            self.velocity.add(Square.gravity)
+        else:
+            self.velocity.y = 0
 
+    def isStable(self):
+        y = (self.position.y + 1) // 10
+        x = self.position.x // 10
+        flag = self.position.x % 10
+        if self.map.pattern[y][x] == 1 or self.map.pattern[y][x + 1] == 1 and not flag:
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Vector import Vector
 from Square import Square
+from Map import Map
 
 
 if __name__ == '__main__':
-    x = Square(Vector(1, 0))
-    print(x)
-    x.jump()
-    print(x)
-
-
+    x = Square(Vector(11, 49), Map())
+    for i in range(20):
+        x.jump()
+        x.move()
+        print(x)
+        print("------------------------")

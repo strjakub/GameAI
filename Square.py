df14cc05bf1abfd4cd4789e79f6dd67d91e279c6
Jakub Strojewski <jakubstr@student.agh.edu.pl>
2022-04-05 21:09:19 +0200

    first engine

--- Square.py
+++ Square.py
@@ -3,13 +3,14 @@ from Map import Map
 
 
 class Square:
-    jumpPower: Vector = Vector(0, -5)
+    jumpPower: Vector = Vector(0, -6)
     gravity: Vector = Vector(0, 1)
 
     def __init__(self, position: Vector, board: Map) -> None:
         self.position = position
         self.map = board
         self.velocity: Vector = Vector(3, 0)
+        self.grid = self.map.to_grid()
 
     def __str__(self) -> str:
         return f"position: {self.position}\nvelocity: {self.velocity}\n{self.is_stable()}\n{self.is_dead()}"
@@ -20,6 +21,8 @@ class Square:
 
     def move(self) -> None:
         self.position.add(self.velocity)
+        while self.grid[self.position.y][self.position.x]:
+            self.position.y -= 1
         if not self.is_stable():
             self.velocity.add(Square.gravity)
         else:
@@ -29,15 +32,20 @@ class Square:
         y = (self.position.y + 1) // 10
         x = self.position.x // 10
         flag = self.position.x % 10
-        if self.map.pattern[y][x] == 1 or self.map.pattern[y][x + 1] == 1 and not flag:
+        if self.map.pattern[y][x] == 1 or (self.map.pattern[y][x + 1] == 1 and not flag):
             return True
         return False
 
     def is_dead(self) -> int:
         grid = self.map.to_grid()
-        if grid[self.position.y][self.position.x] != 0 or\
-                self.position.y - 9 >= 0 and grid[self.position.y - 9][self.position.x] != 0 or\
-                self.position.x + 9 < len(grid[self.position.y]) and grid[self.position.y][self.position.x + 9] != 0 or\
-                self.position.y - 9 >= 0 and self.position.x + 9 < len(grid[self.position.y]) and grid[self.position.y - 9][self.position.x + 9] != 0:
-            return self.position.x
+        for i in range(10):
+            if grid[self.position.y][self.position.x + i] != 0 or \
+                    self.position.y - 9 >= 0 and grid[self.position.y - 9 + i][self.position.x] != 0 or \
+                    self.position.x + 9 < len(grid[self.position.y]) and grid[self.position.y - i][self.position.x + 9] != 0 or \
+                    self.position.y - 9 >= 0 and self.position.x + 9 < len(grid[self.position.y]) and \
+                    grid[self.position.y - 9][self.position.x + 9 - i] != 0:
+                return self.position.x
         return -1
+
+    def contain_square_body(self, x: int, y: int) -> bool:
+        return self.position.x <= x < self.position.x + 10 and self.position.y >= y > self.position.y - 10
--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 from Vector import Vector
 from Square import Square
 from Map import Map
-
+import tkinter
+import keyboard
+import random
 
 if __name__ == '__main__':
     y = Map()
-    for i in y.to_grid():
-        print(i)
-    x = Square(Vector(11, 49), y)
-    for i in range(20):
-        x.jump()
+    x = Square(Vector(5, 49), y)
+    top = tkinter.Tk()
+    grid = y.to_grid()
+    canvas = tkinter.Canvas(top, bg="lightblue", height=len(grid) * 10, width=600)
+    canvas.pack()
+    dash = 0
+    while True:
+        if keyboard.is_pressed('space'):
+            x.jump()
+
         x.move()
-        print(x)
-        print("------------------------")
+        if x.position.x + 60 >= len(grid[0]):
+            break
+
+        canvas.delete("all")
+        for j in range(len(grid)):
+            for i in range(60):
+                if x.contain_square_body(i + dash, j):
+                    flag = random.randint(0, 2)
+                    if flag == 0:
+                        canvas.create_rectangle(i * 10, j * 10, (i + 1) * 10, (j + 1) * 10, fill="green", outline="green")
+                    if flag == 1:
+                        canvas.create_rectangle(i * 10, j * 10, (i + 1) * 10, (j + 1) * 10, fill="lightgreen", outline="lightgreen")
+                    if flag == 2:
+                        canvas.create_rectangle(i * 10, j * 10, (i + 1) * 10, (j + 1) * 10, fill="darkgreen", outline="darkgreen")
+                elif grid[j][dash + i] == 2:
+                    canvas.create_rectangle(i * 10, j * 10, (i + 1) * 10, (j + 1) * 10, fill="red", outline="red")
+                elif grid[j][dash + i] == 1:
+                    canvas.create_rectangle(i * 10, j * 10, (i + 1) * 10, (j + 1) * 10, fill="black", outline="black")
+        canvas.update()
+
+        if x.is_dead() > 0:
+            break
+        dash += 3
+
+    top.mainloop()

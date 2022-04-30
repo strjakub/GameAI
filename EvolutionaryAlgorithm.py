4e537a4c7e23c9bf839efd930bb912de12bbd530
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2022-04-30 22:03:24 +0200

    AI working a bit, some bugs to fix yet

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -1,4 +1,29 @@
+from Generation import Generation
+from Map import Map
+
+
 class EvolutionaryAlgorithm:
-    def __init__(self, map_length):
+    def __init__(self, game_map, map_length, leave_best_num):
+        self.map = game_map
         self.map_length = map_length
-        
+        print(len(self.map.pattern[0]))
+        self.leave_best_num = leave_best_num
+        self.generation = Generation(game_map=self.map, generation_num=50,
+                                     leave_best_num=self.leave_best_num, path_len=self.map_length)
+        self.generation.evaluate()
+        # variables for storing how many moves should be changed before and after death position
+        self.prev_indexes_num = 9
+        self.next_indexes_num = 10
+
+    # return the path to be displayed in the GUI
+    def make_evolution_step(self):
+        self.generation.update_generation(self.prev_indexes_num, self.next_indexes_num)
+        self.generation.evaluate()
+        return self.generation.creatures[self.leave_best_num - 1].path
+
+
+if __name__ == "__main__":
+    EA = EvolutionaryAlgorithm(Map(50), 50, 3)
+    for _ in range(10):
+        print(EA.make_evolution_step())
+
--- Generation.py
+++ Generation.py
@@ -1,11 +1,70 @@
 from Creature import Creature
+from Map import Map
+from Vector import Vector
+from Square import Square
+from queue import PriorityQueue
 
 
 class Generation:
-    def __init__(self, generation_num,  leave_best_num, path_len):
+    def __init__(self, game_map, generation_num,  leave_best_num, path_len):
+        self.map = game_map
         self.generation_num = generation_num
         self.leave_best_num = leave_best_num
-        self.creatures = [Creature(path_len) for _ in range(self.generation_num)]
+        self.path_len = path_len
+        self.frames_per_block = 10
+        self.creatures = [Creature(self.frames_per_block * self.path_len) for _ in range(self.generation_num)]
+        self.best_creatures = PriorityQueue()
 
-    def update_generation(self):
-        ...
+    # evaluate whole generation and return the best creatures, which will be later reproduced
+    def evaluate(self):
+        player = Square(Vector(Map.graininess, (self.map.blocks_height - 1) * Map.graininess - 1), self.map)
+        for creature in self.creatures:
+            player.position = Vector(Map.graininess, (self.map.blocks_height - 1) * Map.graininess - 1)
+            player.alive = True
+            player.jump = False
+            for move in creature.path:
+                if move == 1:
+                    player.jump = True
+
+                player.move()
+                death_pos = player.is_dead()
+                if death_pos != -1:
+                    creature.dist = death_pos // 12 - 1
+                    self.best_creatures.put(creature)
+                    break
+
+            if self.best_creatures.qsize() > self.leave_best_num:
+                self.best_creatures.get()
+
+        self.creatures = []
+        print("QUEUE")
+        while not self.best_creatures.empty():
+            creature = self.best_creatures.get()
+            print(creature)
+            self.creatures.append(creature)
+        print("Check:", self.creatures[0].path == self.creatures[1].path)
+        print("END QUEUE")
+
+    # reproduce best_creatures
+    def update_generation(self, prev_indexes_num, next_indexes_num):
+        reproduction_num = (self.generation_num - self.leave_best_num) // self.leave_best_num
+        for i in range(self.leave_best_num):
+            creature = self.creatures[i]
+            left_range = max(0, creature.dist - prev_indexes_num * self.frames_per_block)
+            right_range = min(self.frames_per_block * self.path_len - 1,
+                              creature.dist + next_indexes_num * self.frames_per_block)
+            creature.create_path(path_len=self.frames_per_block * self.path_len, path=creature.path, generate=True,
+                                 from_index=creature.dist, to_index=right_range)
+            for _ in range(reproduction_num):
+                child = Creature(path_len=self.frames_per_block * self.path_len, path=creature.path, generate=True,
+                                 from_index=left_range, to_index=right_range)
+                self.creatures.append(child)
+
+        while len(self.creatures) < self.generation_num:
+            creature = self.creatures[self.leave_best_num - 1]
+            left_range = max(0, creature.dist - prev_indexes_num * self.frames_per_block)
+            right_range = min(self.frames_per_block * self.path_len - 1,
+                              creature.dist + next_indexes_num * self.frames_per_block)
+            child = Creature(path_len=self.frames_per_block * self.path_len, path=creature.path, generate=True,
+                             from_index=left_range, to_index=right_range)
+            self.creatures.append(child)
